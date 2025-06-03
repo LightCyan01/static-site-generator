06f565d8b2a2ed2c2ba61472840f21c883aa763d
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2025-06-03 08:13:20 -0400

    Adds HTMLNode integration to main demo

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
 def main():
     print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
+    print(HTMLNode(tag="div", value="Hello", children=[], props={"id": "main"}))
 
 if __name__ == "__main__":
     main()
