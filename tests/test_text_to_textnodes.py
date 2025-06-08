1071c8ef911efc706717a5d6722e9e678f3a24c5
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2025-06-08 06:42:44 -0400

    Add text_to_textnodes function and corresponding unit tests for markdown parsing

---
+++ tests/test_text_to_textnodes.py
@@ -0,0 +1,36 @@
+import unittest
+import test_config
+
+from textnode import TextNode, TextType
+from text_to_textnodes import text_to_textnodes
+
+class TestTextToTextnodes(unittest.TestCase):
+    def test_full_markdown_line(self):
+        text = (
+            "This is **text** with an _italic_ word and a `code block` "
+            "and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) "
+            "and a [link](https://boot.dev)"
+        )
+        nodes = text_to_textnodes(text)
+
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode(
+                "obi wan image",
+                TextType.IMAGE,
+                url="https://i.imgur.com/fJRm4Vk.jpeg"
+            ),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, url="https://boot.dev"),
+        ]
+
+        self.assertListEqual(nodes, expected)
+
+if __name__ == "__main__":
+    unittest.main()
