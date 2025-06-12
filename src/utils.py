2a82deca01a55c24f0b3f34030a571fa94087c1a
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2025-06-12 07:10:10 -0400

    Refactor utils.py by removing unused functions and consolidating text node processing

--- src/utils.py
+++ src/utils.py
@@ -1,92 +1,35 @@
 
 from textnode import TextNode, TextType
-import re
+from leafnode import LeafNode
+from split_nodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_nodes = []
-    
-    for node in old_nodes:
-        if node.text_type != TextType.TEXT:
-            new_nodes.append(node)
-            continue
-        else:
-            parts = node.text.split(delimiter)
-            
-            if len(parts) % 2 == 0:
-                raise ValueError(f"Unmatched delimiter {delimiter}")
-            
-            for i, chunk in enumerate(parts):
-                if not chunk:
-                    continue
-                if i % 2 == 0:
-                    new_nodes.append(TextNode(chunk, TextType.TEXT))
-                else:
-                    new_nodes.append(TextNode(chunk, text_type))
-    
-    return new_nodes
+def text_to_children(text):
+    nodes = text_to_textnodes(text)
+    html = [text_node_to_html_node(text_nodes) for text_nodes in nodes]
+    return html
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    return [n for n in nodes if n.text]
 
-def split_nodes_image(old_nodes):
-    new_nodes = []
-    
-    for node in old_nodes:
-        if node.text_type != TextType.TEXT:
-            new_nodes.append(node)
-            continue
-            
-        text = node.text
-        for alt, url in extract_markdown_images(text):
-            marker = f"![{alt}]({url})"
-            parts = text.split(marker, 1)
-            before = parts[0]
-            after = parts[1] if len(parts) > 1 else ""
-            
-            if before:
-                new_nodes.append(TextNode(before, TextType.TEXT))
-            new_nodes.append(TextNode(alt, TextType.IMAGE, url=url))
-            
-            text = after
-            
-        if text:
-            new_nodes.append(TextNode(text, TextType.TEXT))
-            
-    return new_nodes
-
-def split_nodes_link(old_nodes):
-    new_nodes = []
-    
-    for node in old_nodes:
-        if node.text_type != TextType.TEXT:
-            new_nodes.append(node)
-            continue
-            
-        text = node.text
-        for alt, url in extract_markdown_links(text):
-            marker = f"[{alt}]({url})"
-            parts = text. split(marker, 1)
-            before = parts[0]
-            after = parts[1] if len(parts) > 1 else ""
-            
-            if before:
-                new_nodes.append(TextNode(before, TextType.TEXT))
-            new_nodes.append(TextNode(alt, TextType.LINK, url=url))
-            
-            text = after
-        if text:
-            new_nodes.append(TextNode(text, TextType.TEXT))
-
-    return new_nodes
-
-
-def extract_markdown_images(text):
-    match = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
-    return match
-
-def extract_markdown_links(text):
-    match = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
-    return match
-
-
-def markdown_to_blocks(markdown):
-    raw = markdown.split("\n\n")
-    return [block.strip() for block in raw if block.strip()]
+def text_node_to_html_node(text_node: TextNode):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _: 
+            raise ValueError(f"Error: {text_node.text_type}")
