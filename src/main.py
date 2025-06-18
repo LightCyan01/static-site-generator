2a5c6cf4d4f5eaf0cf63707ff6abba5c27c73d67
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2025-06-18 01:23:38 -0400

    update

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
+from utils import generate_page
 from file_utils import copy_static_to_public
 
 def main():
+    copy_static_to_public("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
     
-    pass
-
 if __name__ == "__main__":
-    copy_static_to_public()
     main()
