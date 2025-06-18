5152f1bac2a9383b1cf043ee4a235ff13bdabd95
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2025-06-18 01:23:21 -0400

    Refactor generate_page function to improve readability and structure

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-
+import os
 from textnode import TextNode, TextType
 from leafnode import LeafNode
 from split_nodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
@@ -35,4 +35,32 @@ def text_node_to_html_node(text_node: TextNode):
             raise ValueError(f"Error: {text_node.text_type}")
 
 def generate_page(from_path, template_path, dest_path):
+    from parser import markdown_to_html_node
+    from extractors import extract_title
+
+    #1. log
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    
+    #2. read markdown
+    with open(from_path, "r") as f:
+        md = f.read()
+        
+    #3. read template
+    with open(template_path, "r") as f:
+        template = f.read()
+        
+    #4. convert markdown to HTML string
+    root = markdown_to_html_node(md)
+    content_html = root.to_html()
+    
+    #5. extract h1 as title
+    title = extract_title(md)
+    
+    #6. add title & content to template
+    page = (template.replace("{{ Title }}", title).replace("{{ Content }}", content_html))
+    
+    #7. write results
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    with open(dest_path, "w") as f:
+        f.write(page)
     
