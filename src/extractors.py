e7ecc9187b158cec6aacacdd09e6948e7b26c4ac
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2025-06-17 21:36:25 -0400

    Add extract_title function to extract H1 headers from markdown

--- src/extractors.py
+++ src/extractors.py
@@ -7,3 +7,10 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     match = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return match
+
+def extract_title(markdown: str):
+    for line in markdown.splitlines():
+        stripped_line = line.strip()
+        if(stripped_line.startswith("# ")):
+            return stripped_line[2:].strip()
+        raise ValueError("No H1 header found")
