04cc19bcb175ec3796cf192a2e3ec800132d01b6
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2025-06-18 01:53:18 -0400

    Remove unused images and update static file copy destination in main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from file_utils import copy_static_to_public
 
 def main():
     basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
-    copy_static_to_public("static", "public")
+    copy_static_to_public("static", "docs")
     generate_pages_recursive(dir_path_content="content", template_path="template.html", dest_dir_path="docs", basepath=basepath)
     
 if __name__ == "__main__":
